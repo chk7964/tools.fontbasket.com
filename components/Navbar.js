3086d78f48cfcdf8ed9ca7426877f67993885160
chk7964 <chk7964@yahoo.com>
2022-09-28 23:45:02 +0530

    vs

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,15 +9,15 @@ export default function Navbar() {
 <div className="scrollmenu">
   <div className="menu">
   <a href="./">Home</a>
-  <a href="/capitalize-case">CapitalizeCase Converter</a>
-  <a href="/fancy-text">Fancy Text</a>
-  <a href="/invisible-character/">Invisible Text Generator</a>  
-  <a href="/lower-case/">LowerCase Text Converter</a>
-  <a href="/numbers-to-words-converter/">Number To Word Converter</a>
-  <a href="/reverse-text/">Reverse Text Converter</a>
-  <a href="/shuffle-text/">Suffle Text </a>
-  <a href="/text-repeater/">Text Repeater</a>
-  <a href="/upper-case/">UpperCase Conveter</a>
+  <a href="./capitalize-case">CapitalizeCase Converter</a>
+  <a href="./fancy-text">Fancy Text</a>
+  <a href="./invisible-character/">Invisible Text Generator</a>  
+  <a href="./lower-case/">LowerCase Text Converter</a>
+  <a href="./numbers-to-words-converter/">Number To Word Converter</a>
+  <a href="./reverse-text/">Reverse Text Converter</a>
+  <a href="./shuffle-text/">Suffle Text </a>
+  <a href="./text-repeater/">Text Repeater</a>
+  <a href="./upper-case/">UpperCase Conveter</a>
 
 </div>
 </div>
