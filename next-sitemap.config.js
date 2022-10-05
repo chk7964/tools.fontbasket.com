1da8aa3ae6d75f8e5ef0a32d84f56cf7ce378e3d
chk7964 <chk7964@yahoo.com>
2022-10-05 20:20:28 +0530

    vs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://tools.fontbasket.com',
   generateRobotsTxt: true, // (optional)
-  changefreq: 'daily',
+  changefreq: 'weekly',
   priority: 0.5,
   sitemapSize: 5000,
   generateRobotsTxt: true,
