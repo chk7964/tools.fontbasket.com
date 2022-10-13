51e5b17bc72c7f4121970af4a1bcdf7ecf08f0e6
chk7964 <chk7964@yahoo.com>
2022-10-13 17:23:13 +0530

    vs

--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,18 @@
+import React from 'react';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default function Test() {
-
-    const onClick = () => toast('Toast is good', { hideProgressBar: true, autoClose: 2000, type: 'success' })
-
-    return (<>
-    <button onClick={onClick}> Click Me</button>
-    <ToastContainer />
-    </>
+export default function About() {
+    const notify = (e) =>  {
+ toast("Wow so easy!");
+        console.log('Free pizza!');
+    }
+    return (
+     <>
+ <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>   </>
     )
-
-}
+   }
