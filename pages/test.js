0c94f6f868494fc8e4762664888950d51207a99b
chk7964 <chk7964@yahoo.com>
2022-10-13 23:06:31 +0530

    vs

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { ToastContainer, toast } from '@martinadamec/react-toastify';
-import '@martinadamec/react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function About() {
     const notify = (e) =>  {
