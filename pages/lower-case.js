63387a000b47443b1aac2c7f3e7e8a725e667089
chk7964 <chk7964@yahoo.com>
2022-10-13 09:39:38 +0530

    vs

--- pages/lower-case.js
+++ pages/lower-case.js
@@ -5,37 +5,37 @@ import ClipboardJS from 'clipboard';
 import $ from "jquery";
 // import React from 'react';
 
-const copy = () =>  {     
-
-  var clipboard = new ClipboardJS('.bt');
-
-  clipboard.on('success', function (e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    // toast["success"]("Text Is Copied " + e.text)
-  
-    e.clearSelection();
-    clipboard.destroy();
-  });
-  
-  clipboard.on('error', function (e) {
-    // toast["warning"]("Output Is Empty");
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-    clipboard.destroy();
-  });
-}
-
-const handleClick = () =>  { 
-  let str = document.getElementById("k").value;
-  var string = str.toLowerCase();
-  console.log(string);
-  $("#result").html(string); 
-}
 
 export default function lowercase() {
+  const copy = () =>  {     
 
+    var clipboard = new ClipboardJS('.bt');
+  
+    clipboard.on('success', function (e) {
+      console.info('Action:', e.action);
+      console.info('Text:', e.text);
+      console.info('Trigger:', e.trigger);
+      // toast["success"]("Text Is Copied " + e.text)
+    
+      e.clearSelection();
+      clipboard.destroy();
+    });
+    
+    clipboard.on('error', function (e) {
+      // toast["warning"]("Output Is Empty");
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+      clipboard.destroy();
+    });
+  }
+  
+  const handleClick = () =>  { 
+    let str = document.getElementById("k").value;
+    var string = str.toLowerCase();
+    console.log(string);
+    $("#result").html(string); 
+  }
+  
  return (
   <>
   <Head>
