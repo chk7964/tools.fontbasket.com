ebef940a43908618c700901fa0d6f08c2e83b90e
chk7964 <chk7964@yahoo.com>
2022-10-13 17:29:33 +0530

    vs

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 
 import React, { isValidElement, useRef, useLayoutEffect, useEffect, cloneElement, useReducer, useState, forwardRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function About() {
     const notify = (e) =>  {
