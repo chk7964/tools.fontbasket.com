4f1dec1188b0dfd303ff176c0bdb220c7f09df9c
chk7964 <chk7964@yahoo.com>
2022-10-13 20:05:10 +0530

    vs

--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,17 @@
-import Toast from 'awesome-toast-component'
+import React from 'react';
+
+import { ToastContainer, toast } from '@martinadamec/react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function About() {
     const notify = (e) =>  {
-      new Toast('Hello world! 👋');
+      toast("Wow so easy !");
         console.log('Free pizza!');
     }
     return (
      <>
  <div>
+ <ToastContainer />
         <button onClick={notify}>Notify!</button>
         {/* <ToastContainer /> */}
       </div>   </>
