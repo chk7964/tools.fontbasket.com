599aa56a20eb4c77a6c46453125b09c7decc5dea
chk7964 <chk7964@yahoo.com>
2022-10-14 20:07:16 +0530

    vs

--- pages/upper-case.js
+++ pages/upper-case.js
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ClipboardJS from 'clipboard';
 export default function uppercase() {
-  
+
   const copy = () =>  {     
 
     var clipboard = new ClipboardJS('.bt');
@@ -47,9 +47,9 @@ export default function uppercase() {
   <div className="text-center">
     <button className="bg-green-500 rounded text-white p-2 px-4" id="b"> Convert </button>
   </div> */}
-  <div className="p-1">
+  {/* <div className="p-1">
     <textarea  className="border border-red-500 w-full rounded p-2" rows="5" id="result" placeholder="Your Result Here"></textarea>
-  </div>
+  </div> */}
   <div className="text-center">
     <button onClick={copy} className="bg-green-500 rounded text-white p-2 px-4 bt" data-clipboard-target="#result"> Copy</button>
   </div>
@@ -59,13 +59,13 @@ export default function uppercase() {
       
   
 
-    <br/>
+    {/* <br/>
     <h1 className='text-xl font-bold'>What Is Upper Case Text Generator?</h1>
     <p className="italic">Hey user, If you want to convert your text in uppercase then you are on the right website. Here you can convert your any type text in Upper Case Text. </p>
   <br/>
   <h1 className='text-xl font-bold'>How To Use Upper Case Text Generator?</h1>
     <p className="italic">Here i will tell you how to use this tool. First for you copy your text who want to upper Case convert and paste or type here "Enter Your Text" box. Then you click convert button and see result in </p>
-  
+   */}
   </>
  )
 }
