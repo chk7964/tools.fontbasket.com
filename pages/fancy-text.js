3e8f371d1a75dad6d8acd23e8f6b3cba3bc3e5dc
chk7964 <chk7964@yahoo.com>
2022-10-26 16:56:46 +0530

    vs

--- pages/fancy-text.js
+++ pages/fancy-text.js
@@ -7,7 +7,7 @@ export default function fancytext() {
 <Head>
     <title>Fancy Text Generator</title>
 </Head>
- <script src="https://code.jquery.com/jquery-3.6.1.min.js" async/>
+ <Script src="https://code.jquery.com/jquery-3.6.1.min.js" async/>
 <style jsx>{`
      .copybutton{
         cursor:pointer;
