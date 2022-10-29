37e7cdc9bd4eb965baba81316303327521a2742a
chk7964 <chk7964@yahoo.com>
2022-10-29 16:43:50 +0530

    vs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,23 @@ module.exports = {
   sitemapSize: 5000,
   generateRobotsTxt: true,
   exclude: ['/test'],
+
+
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, 'kruti-dev/kruti-dev-to-unicode'),
+    await config.transform(config, 'kruti-dev/unicode-to-kruti-dev')
+  ],
+
+  
   // ...other options
   robotsTxtOptions: {
     policies: [
@@ -20,4 +37,7 @@ module.exports = {
       }
     ]
   }
+
+
+  
 }
